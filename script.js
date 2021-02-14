38c4cc5d2e2fcaaac28b6ed63f69c3a6df59b933
Andrew Dunn <dunnand@oregonstate.edu>
2021-02-14 00:43:01 -0600

    Click EventListeners added to all calculator buttons. On button click, internal display value will be updated, then shown on calculator display

--- script.js
+++ script.js
@@ -6,10 +6,25 @@ let multiply = (num1, num2) => num1 * num2;
 
 let divide = (num1, num2) => num1 / num2;
 
+let buttonEnteredValues = [];
+let displayValue = 0;
+let display = document.getElementById("displayDigits");
+
+addButtonActions();
+
+
+function addButtonActions(){
+    let buttons = document.getElementsByClassName("calculatorButton");
+    let i;
+    for(i = 0; i < buttons.length; i++) {
+            buttons[i].addEventListener("click", pressButton);
+    }
+    
+}
+
 function operate(operator, num1, num2) {
     switch(operator){
         case '+':
-            console.log('hello');
             return add(num1, num2);
         case '-':
             subtract(num1, num2);
@@ -21,4 +36,18 @@ function operate(operator, num1, num2) {
     }
 }
 
-console.log(operate('+',3,4));
+function pressButton() {
+    buttonEnteredValues.push(this.innerHTML);
+    console.log(buttonEnteredValues);
+    updateDisplayValue();
+    updateDisplay();
+}
+
+function updateDisplayValue() {
+    displayValue = buttonEnteredValues.join("");
+    displayValue = parseInt(displayValue);
+}
+
+function updateDisplay() {
+    display.innerHTML = displayValue;
+}
