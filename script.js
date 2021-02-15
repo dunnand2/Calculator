9ac1a4b7fd06f463c1102babac79ba1cc4ba29c6
Andrew Dunn <dunnand@oregonstate.edu>
2021-02-15 15:53:19 -0600

    Calculator and display working for integers. Floats currently not working

--- script.js
+++ script.js
@@ -1,25 +1,31 @@
 let add = (num1, num2) => num1 + num2;
-
 let subtract = (num1, num2) => num1 - num2;
-
 let multiply = (num1, num2) => num1 * num2;
-
 let divide = (num1, num2) => num1 / num2;
 
 let buttonEnteredValues = [];
-let displayValue = 0;
+let displayValue = null;
 let display = document.getElementById("displayDigits");
+let operand1 = null;
+let operand2 = null;
+let currentOperator;
 
 addButtonActions();
 
 
 function addButtonActions(){
-    let buttons = document.getElementsByClassName("calculatorButton");
-    let i;
-    for(i = 0; i < buttons.length; i++) {
-            buttons[i].addEventListener("click", pressButton);
+    let numericalButtons = document.getElementsByClassName("numericalButton");
+    let operatorButtons = document.getElementsByClassName("operatorButton");
+    let equalsButton = document.getElementById("equalsButton");
+    let clearButton = document.getElementById("clearButton"); 
+    for(let i = 0; i < numericalButtons.length; i++) {
+            numericalButtons[i].addEventListener("click", pressNumericalButton);
     }
-    
+    for(let i = 0; i < operatorButtons.length; i++) {
+        operatorButtons[i].addEventListener("click", pressOperatorButton);
+    }
+    equalsButton.addEventListener("click", pressEqualsButton);
+    clearButton.addEventListener("click", clearAll);
 }
 
 function operate(operator, num1, num2) {
@@ -27,18 +33,16 @@ function operate(operator, num1, num2) {
         case '+':
             return add(num1, num2);
         case '-':
-            subtract(num1, num2);
-            break;
+            return subtract(num1, num2);
         case '*':
-            multiply(num1, num2);
+            return multiply(num1, num2);
         case '/':
-            divide(num1, num2);
+            return divide(num1, num2);
     }
 }
 
-function pressButton() {
+function pressNumericalButton() {
     buttonEnteredValues.push(this.innerHTML);
-    console.log(buttonEnteredValues);
     updateDisplayValue();
     updateDisplay();
 }
@@ -50,4 +54,72 @@ function updateDisplayValue() {
 
 function updateDisplay() {
     display.innerHTML = displayValue;
+}
+
+function pressOperatorButton() {
+    // if no calculations have been performed, update operand1 and set operator
+    if(operand1 == null) {
+        setOperand1();
+        setOperator(this.value);
+        return;
+    }
+    else {
+        // Update operator if null, or if no numbers entered after last operator pressed
+        if(!currentOperator || (buttonEnteredValues.length < 1)){
+            currentOperator = this.value;
+            return;
+        }
+        operand2 = displayValue;
+        displayValue = operate(currentOperator, operand1, operand2);
+        updateDisplay();
+        clearCurrentNumbersArray();
+        operand1 = displayValue;
+        currentOperator = this.value;
+        operand2 = null;
+    }
+
+}
+
+function setOperand1() {
+    operand1 = displayValue;
+    clearCurrentNumbersArray();
+    displayValue = null;
+}
+
+function clearCurrentNumbersArray() {
+    buttonEnteredValues.length = 0;
+}
+
+function pressEqualsButton() {
+    console.log(buttonEnteredValues.length)
+    console.log(currentOperator)
+    if(operand1 == null) {
+        return;
+    }
+    else {
+        if(displayValue && currentOperator && (buttonEnteredValues.length > 0)) {
+            operand2 = displayValue;
+            displayValue = operate(currentOperator, operand1, operand2);
+            updateDisplay();
+            clearCurrentNumbersArray();
+            operand1 = displayValue;
+            currentOperator = null;
+            operand2 = null;
+        }
+    }
+}
+
+function clearAll(){
+    // resets display, operators, operands, and entered number array 
+    displayValue = 0;
+    operand1 = null;
+    operand2 = null;
+    setOperator(null);
+    clearCurrentNumbersArray();
+    updateDisplay();
+}
+
+
+function setOperator(value) {
+    currentOperator = value;
 }
